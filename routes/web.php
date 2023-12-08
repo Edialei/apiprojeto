2c96cdb72c19aa3a76ad9173f4e16d7631ce478c
Edialei <edialei61@gmail.com>
2023-12-08 17:56:37 -0300

    login

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@ use Illuminate\Support\Facades\Route;
 /* Route::prefix('login')->group(function () {
     Route::get('/', [LoginController::class, 'index'])->name('login.index');
     Route::post('/', [LoginController::class, 'store'])->name('login.store');
-    Route::get('/destroy', [LoginController::class, 'destroy'])->name('login.destroy');
 });  */
 
 Route::post('/api/login', [LoginController::class, 'store'])->name('api.login');
+Route::get('/destroy', [LoginController::class, 'destroy'])->name('login.destroy');
 
 //Route::get('/register', [UserController::class, 'create'])->name('register');
 Route::post('/api/registro', [UserController::class, 'store'])->name('api.regsitro');
